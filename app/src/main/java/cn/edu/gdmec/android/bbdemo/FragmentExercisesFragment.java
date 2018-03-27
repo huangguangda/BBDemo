61b378e5bf010ad817a0c460439689e1265ee738
huangguangda <2397923107@qq.com>
2018-03-27 16:08:21 +0800

    搞定了

---
+++ app/src/main/java/cn/edu/gdmec/android/bbdemo/FragmentExercisesFragment.java
@@ -0,0 +1,25 @@
+package cn.edu.gdmec.android.bbdemo;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.os.Bundle;
+
+
+public class FragmentExercisesFragment extends Fragment  {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_exercises, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+}
--- app/src/main/java/cn/edu/gdmec/android/bbdemo/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/bbdemo/MainActivity.java
@@ -1,32 +1,142 @@
 package cn.edu.gdmec.android.bbdemo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements View.OnClickListener{
     private TextView et_user_name;
+   
+    private TextView tv_back;
+    private TextView tv_main_title;
+    private TextView tv_save;
+    private RelativeLayout title_bar;
+    private RelativeLayout main_body;
+    private TextView bottom_bar_text_course;
+    private ImageView bottom_bar_image_course;
+    private RelativeLayout bottom_bar_course_btn;
+    private TextView bottom_bar_text_exercise;
+    private ImageView bottom_bar_image_exercise;
+    private RelativeLayout bottom_bar_exercise_btn;
+    private TextView bottom_bar_text_myinfo;
+    private ImageView bottom_bar_image_myinfo;
+    private RelativeLayout bottom_bar_myinfo_btn;
+    private LinearLayout main_bottom_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         et_user_name = findViewById(R.id.et_user_name);
-        Intent intent=new Intent(MainActivity.this,LoginActivity.class);
-        startActivityForResult(intent,1);
+        /*Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivityForResult(intent, 1);
+*/
+        setMain();
+    }
+
+    private void setSelectStatus(int index){
+        switch (index){
+            case 0:
+                bottom_bar_image_course.setImageResource(R.drawable.main_course_icon_selected);
+                bottom_bar_text_course.setTextColor(Color.parseColor("#0097F7"));
+
+                bottom_bar_text_exercise.setTextColor(Color.parseColor("#666666"));
+                bottom_bar_text_myinfo.setTextColor(Color.parseColor("#666666"));
+
+                bottom_bar_image_exercise.setImageResource(R.drawable.main_exercises_icon);
+                bottom_bar_image_myinfo.setImageResource(R.drawable.main_my_icon);
+                break;
+            case 1:
+                bottom_bar_image_exercise.setImageResource(R.drawable.main_exercises_icon_selected);
+                bottom_bar_text_exercise.setTextColor(Color.parseColor("#0097F7"));
+
+                bottom_bar_text_course.setTextColor(Color.parseColor("#666666"));
+                bottom_bar_text_myinfo.setTextColor(Color.parseColor("#666666"));
+
+                bottom_bar_image_course.setImageResource(R.drawable.main_course_icon);
+                bottom_bar_image_myinfo.setImageResource(R.drawable.main_my_icon);
+                break;
+            case 2:
+                bottom_bar_image_myinfo.setImageResource(R.drawable.main_my_icon_selected);
+                bottom_bar_text_myinfo.setTextColor(Color.parseColor("#0097F7"));
+
+                bottom_bar_text_course.setTextColor(Color.parseColor("#666666"));
+                bottom_bar_text_exercise.setTextColor(Color.parseColor("#666666"));
+
+                bottom_bar_image_exercise.setImageResource(R.drawable.main_exercises_icon);
+                bottom_bar_image_course.setImageResource(R.drawable.main_course_icon);
+                break;
+        }
+    }
+
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            String userName = data.getStringExtra("userName");
+            //if (!TextUtils.isEmpty(userName)){
+            Toast.makeText(MainActivity.this, "登陆成功：" + userName, Toast.LENGTH_SHORT).show();
+            et_user_name.setText(userName);
+            //}
+        }
+    }*/
+
+    private void setMain() {
+        this.getSupportFragmentManager().beginTransaction().add(R.id.main_body,new FragmentCourseFragment()).commit();
+        setSelectStatus(0);
+    }
+
+    private void initView() {
+        tv_back = findViewById(R.id.tv_back);
+        tv_main_title = findViewById(R.id.tv_main_title);
+        tv_save = findViewById(R.id.tv_save);
+        title_bar = findViewById(R.id.title_bar);
+
+        main_body = findViewById(R.id.main_body);
+        bottom_bar_text_course = findViewById(R.id.bottom_bar_text_course);
+        bottom_bar_image_course = findViewById(R.id.bottom_bar_image_course);
+        bottom_bar_course_btn = findViewById(R.id.bottom_bar_course_btn);
+        bottom_bar_text_exercise = findViewById(R.id.bottom_bar_text_exercises);
+        bottom_bar_image_exercise = findViewById(R.id.bottom_bar_image_exercises);
+        bottom_bar_exercise_btn = findViewById(R.id.bottom_bar_exercises_btn);
+        bottom_bar_text_myinfo = findViewById(R.id.bottom_bar_text_myinfo);
+        bottom_bar_image_myinfo = findViewById(R.id.bottom_bar_image_myinfo);
+        bottom_bar_myinfo_btn = findViewById(R.id.bottom_bar_myinfo_btn);
+        main_bottom_bar = findViewById(R.id.main_bottom_bar);
+
+        bottom_bar_course_btn.setOnClickListener(this);
+        bottom_bar_exercise_btn.setOnClickListener(this);
+        bottom_bar_myinfo_btn.setOnClickListener(this);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (data!=null){
-            String userName = data.getStringExtra("userName");
-            //if (!TextUtils.isEmpty(userName)){
-            Toast.makeText(MainActivity.this,"登陆成功："+userName, Toast.LENGTH_SHORT).show();
-                et_user_name.setText(userName);
-            //}
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bottom_bar_course_btn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_body,new FragmentCourseFragment()).commit();
+                setSelectStatus(0);
+                break;
+            case R.id.bottom_bar_exercises_btn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_body,new FragmentExercisesFragment()).commit();
+                setSelectStatus(1);
+                break;
+            case R.id.bottom_bar_myinfo_btn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_body,new FragmentMyinfoFragment()).commit();
+                setSelectStatus(2);
+                break;
         }
     }
 }
